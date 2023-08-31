77ddf3b99bb5604475226c5de3f9b88909a73ef7
Victoria Amare <victoriaoliviaamare@gmail.com>
2023-08-31 13:12:14 -0500

    javascript

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("who are your favorite front end dev youtubers?");
+var contentDiv = document.getElementById ("content");
+var textNode = document.createTextNode ("this text was added using JavaScript!");
+contentDiv.appendChild(textNode);
