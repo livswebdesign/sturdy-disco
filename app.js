f150876b349c984297e8d2655c30e80e783ae83b
Victoria Amare <victoriaoliviaamare@gmail.com>
2023-09-17 19:34:07 -0500

    project 2

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
-console.log("who are your favorite front end dev youtubers?");
-var contentDiv = document.getElementById ("content");
-var textNode = document.createTextNode ("this text was added using JavaScript!");
-contentDiv.appendChild(textNode);
+console.log("hello mrs.escobar!");
+
+// This is a global variable
+let globalVariable = 7;
+
+  // This is a block variable
+  let blockVariable = 14;
+  console.log("Block variable:", blockVariable);
+
+
+// Task 4: Perform a mathematical operation on variables
+let num1 = 20;
+let num2 = 30;
+let result = num1 + num2;
+console.log("Result of addition:", result);
+
+let myString = "This is a string variable.";
+
+console.log(myString);
+
+const myObject = {
+  key1: "value1",
+  key2: "value2",
+};
+
+console.log("Object variable:", myObject);
